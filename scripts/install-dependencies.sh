f7a3e29e06685de6a403e8a78f53da0a0ec82781
laurfb <laurfb@gmail.com>
2023-02-11 13:58:58 +0200

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 sudo apt-get install -y tclsh pkg-config cmake libssl-dev build-essential ffmpeg
-git clone --depth 1 --branch v1.5.0 https://github.com/Haivision/srt.git libsrt
+git clone --depth 1 --branch v1.5.1 https://github.com/Haivision/srt.git libsrt
 cd libsrt
 ./configure
 make -j
